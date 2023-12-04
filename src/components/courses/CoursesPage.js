4d54ddbd68d042e8d01e56c6656a6738905a764a
rruiz88 <roman.ruiz816@gmail.com>
2023-12-04 13:44:46 -0600

    course form and inputs created

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,17 +5,24 @@ import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
+import { Redirect } from "react-router-dom";
 
 class CoursesPage extends React.Component {
+  state = {
+    redirectToAddCoursePage: false
+  };
+
   componentDidMount() {
-    if (this.props.courses.length === 0) {
-      this.props.actions.loadCourses().catch((error) => {
+    const { courses, authors, actions } = this.props;
+
+    if (courses.length === 0) {
+      actions.loadCourses().catch(error => {
         alert("Loading courses failed" + error);
       });
     }
 
-    if (this.props.authors.length === 0) {
-      this.props.actions.loadAuthors().catch((error) => {
+    if (authors.length === 0) {
+      actions.loadAuthors().catch(error => {
         alert("Loading authors failed" + error);
       });
     }
@@ -24,7 +31,17 @@ class CoursesPage extends React.Component {
   render() {
     return (
       <>
+        {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
         <h2>Courses</h2>
+
+        <button
+          style={{ marginBottom: 20 }}
+          className="btn btn-primary add-course"
+          onClick={() => this.setState({ redirectToAddCoursePage: true })}
+        >
+          Add Course
+        </button>
+
         <CourseList courses={this.props.courses} />
       </>
     );
@@ -34,7 +51,7 @@ class CoursesPage extends React.Component {
 CoursesPage.propTypes = {
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
@@ -42,14 +59,13 @@ function mapStateToProps(state) {
     courses:
       state.authors.length === 0
         ? []
-        : state.courses.map((course) => {
+        : state.courses.map(course => {
             return {
               ...course,
-              authorName: state.authors.find((a) => a.id === course.authorId)
-                .name,
+              authorName: state.authors.find(a => a.id === course.authorId).name
             };
           }),
-    authors: state.authors,
+    authors: state.authors
   };
 }
 
@@ -57,9 +73,12 @@ function mapDispatchToProps(dispatch) {
   return {
     actions: {
       loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
-      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
-    },
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+    }
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoursesPage);
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,15 +1,19 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
 
-export function createCourse(courses) {
-  return { type: types.CREATE_COURSE, courses };
-}
-
 export function loadCourseSuccess(courses) {
   return {
     type: types.LOAD_COURSES_SUCCESS,
     courses,
   };
+}
+
+export function createCourseSuccess(courses) {
+  return { type: types.CREATE_COURSE_SUCCESS, courses };
+}
+
+export function updateCourseSuccess(courses) {
+  return { type: types.UPDATE_COURSE_SUCCESS, courses };
 }
 
 export function loadCourses() {
@@ -18,6 +22,21 @@ export function loadCourses() {
       .getCourses()
       .then((courses) => {
         dispatch(loadCourseSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(courses) {
+  return function (dispatch) {
+    return courseApi
+      .saveCourse(courses)
+      .then((savedCourse) => {
+        courses.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
       })
       .catch((error) => {
         throw error;
